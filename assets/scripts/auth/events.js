d0d3ac7e592c4188859cd2a025ff76760ff848ad
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-06 09:32:01 -0400

    Add sign up functionality
    Add sign-up functionality in events.js, api.js, and ui.js

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,14 +1,20 @@
 'use strict';
 
 const getFormFields = require('../../..//lib/get-form-fields');
+const api = require('./api');
+const ui = require('./ui');
 
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp);
 };
 
-const onSignUp = () => {
+const onSignUp = (event) => {
   event.preventDefault();
-  console.log('clicked sign up');
+  let data = getFormFields(event.target);
+  console.log(data);
+  api.signUp(data)
+    .done(ui.success)
+    .fail(ui.failure);
 }
 
 module.exports = {
