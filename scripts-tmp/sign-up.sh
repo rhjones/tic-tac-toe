35386c21f0970bec31703a4409d54908a27676c0
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-06 09:31:30 -0400

    tweak sign-up curl script
    Add echo statement and allow for variables

--- scripts-tmp/sign-up.sh
+++ scripts-tmp/sign-up.sh
@@ -1,9 +1,23 @@
-curl --include --request POST http://tic-tac-toe.wdibos.com/sign-up \
+# curl --include --request POST 'http://ttt.wdibos.com/sign-up' \
+#   --header "Content-Type: application/json" \
+#   --data '{
+#     "credentials": {
+#       "email": "kigali@kampala.com",
+#       "password": "kampala",
+#       "password_confirmation": "kampala",
+#     }
+#   }'
+
+curl "http://tic-tac-toe.wdibos.com/sign-up" \
+  --include \
+  --request POST \
   --header "Content-Type: application/json" \
-  --data '{
-    "credentials": {
-      "email": "kigali@kampala.com",
-      "password": "pwd",
-      "password_confirmation": "pwd",
+  --data "{
+    \"credentials\": {
+      \"email\": \"${EMAIL}\",
+      \"password\": \"${PASSWORD}\",
+      \"password_confirmation\": \"${PASSWORD}\"
     }
-  }'
+  }"
+
+echo
