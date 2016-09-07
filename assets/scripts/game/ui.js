1a72a0751a664054d31daf3588df3dfa2b53f7b6
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-07 10:22:27 -0400

    Check for horizontal wins
    Add checkRows() functionality in ui.js

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -22,16 +22,24 @@ const isEmpty = (id) => {
   return (!app.game.cells[id]);
 };
 
-const checkRows = (id) => {
+const checkRows = (cells, id) => {
   console.log('checking rows for cell', id);
+  if ([0, 1, 2].indexOf(id) > -1) {
+    return ((cells[0] === cells[1]) && (cells[1] === cells[2]));
+  }
+  if ([3, 4, 5].indexOf(id) > -1) {
+    return ((cells[2] === cells[4]) && (cells[4] === cells[5]));
+  }
+  if ([6, 7, 8].indexOf(id) > -1) {
+    return ((cells[6] === cells[7]) && (cells[7] === cells[8]));
+  }
 };
 
 const checkCols = (id) => {
   console.log('checking cols for cell', id);
 };
 
-const checkDiagonals = (id) => {
-  let cells = app.game.cells;
+const checkDiagonals = (cells, id) => {
   let win = false;
   if ([0, 4, 8].indexOf(id) > -1) {
     win = (cells[0] === cells[4]) && (cells[4] === cells[8]);
@@ -45,12 +53,17 @@ const checkDiagonals = (id) => {
 };
 
 const checkWin = (id) => {
+  let cells = app.game.cells;
   console.log('checking for a win using cell', id);
-  checkRows(id);
-  checkCols(id);
-  if ([0, 2, 4, 6, 8].indexOf(id) > -1) {
-    if(checkDiagonals(id)) {
-      console.log(app.game.cells[id], 'won!');
+  if (checkRows(cells, id)) {
+    return true;
+  }
+  else if (checkCols(cells, id)) {
+    return true;
+  }
+  else if ([0, 2, 4, 6, 8].indexOf(id) > -1) {
+    if(checkDiagonals(cells, id)) {
+      return true;
     }
   }
 };
@@ -63,9 +76,6 @@ const markCell = (id) => {
   } else if (!app.xTurn) {
     cell.html('o');
   }
-  if(app.currentGameMoves >= 5) {
-    checkWin(id);
-  }
   app.xTurn = !app.xTurn;
 };
 
@@ -73,8 +83,15 @@ const takeTurnSuccess = (data) => {
   console.log(data);
   app.game = data.game;
   app.currentGameMoves++;
+  let id = app.currentCellId;
   console.log('current moves', app.currentGameMoves);
-  markCell(app.currentCellId);
+  markCell(id);
+  if(app.currentGameMoves >= 5) {
+    checkWin(id);
+  }
+  if (checkWin(id)) {
+    console.log('game is won!');
+  }
 };
 
 module.exports = {
