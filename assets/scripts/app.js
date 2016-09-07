3df33a58a1ebb958b1bc3ebb6455d3e10ed0934d
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-07 11:55:09 -0400

    Move game data out of app
    Create a new game object in assets/scripts/game/game.js
    
    Use this object to store game data

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,6 @@
 
 const app = {
   host: 'http://tic-tac-toe.wdibos.com',
-  xTurn: true,
-  currentGameMoves: 0,
 };
 
 module.exports = app;
---
+++ assets/scripts/game/game.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const game = {
+  xTurn: true,
+  currentGameMoves: 0,
+};
+
+module.exports = game;
