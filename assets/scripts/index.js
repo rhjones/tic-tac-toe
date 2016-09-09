539a64309f213dcc7459d1aea0a6987144303681
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-09 13:38:22 -0400

    Comment out reference to example.js
    Comment out reference to example.js in index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,7 +4,7 @@
 // var example = require('./example');
 
 // use require without a reference to ensure a file is bundled
-require('./example');
+// require('./example');
 
 const authEvents = require('./auth/events.js');
 const gameEvents = require('./game/events.js');
