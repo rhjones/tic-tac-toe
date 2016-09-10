97462c4851ecb3cc071378150ad6e223eeec6cc4
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-10 19:09:37 -0400

    Add 'alerts' object to app
    Add 'alerts' object within app to hold all messages to the user

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,36 @@
 
 const app = {
   host: 'https://aqueous-atoll-85096.herokuapp.com',
+  alerts: {
+    passwordChanged: {
+      class: 'success',
+      msg: '<i class="fa fa-lock" aria-hidden="true"></i> Your password has been changed.',
+    },
+    signUpFail: {
+      class: 'warning',
+      msg: '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> Unable to create account.',
+    },
+    logInFail: {
+      class: 'warning',
+      msg: '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> Unable to log in.',
+    },
+    logOutFail: {
+      class: 'warning',
+      msg: '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> Unable to log out.',
+    },
+    createGameFail: {
+      class: 'warning',
+      msg: '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> Unable to create a new game.',
+    },
+    endGameFail: {
+      class: 'warning',
+      msg: '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> Unable to mark game as completed.',
+    },
+    turnFail: {
+      class: 'warning',
+      msg: '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> Unable to save your move.',
+    },
+  },
 };
 
 module.exports = app;
