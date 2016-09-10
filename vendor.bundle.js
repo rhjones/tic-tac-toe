142598f7ef73f0549cf10e4e6b2d83ea42a8911b
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-10 12:42:15 -0400

    Rebuild
    Remove .woff2 file (using Font Awesome CDN instead)
    Rerun grunt build

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(34);
+	module.exports = __webpack_require__(28);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 34:
+/***/ 28:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
