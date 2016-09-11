b762399df1c0761ecb6ba1b0d82be2571324b078
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-10 20:02:17 -0400

    Add game-related messages to app.alerts
    Add welcome, x win, o win, and tie game messages to app.alerts

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,19 @@
 const app = {
   host: 'https://aqueous-atoll-85096.herokuapp.com',
   alerts: {
+    welcome: {
+      class: 'welcome',
+      msg: '<span class="larger">Welcome!</span><br/>Click "New Game" to get started.',
+    },
+    xWin: {
+      msg: '<h2><i class="fa fa-circle x" aria-hidden="true"></i> wins!</h2>',
+    },
+    yWin: {
+      msg: '<h2><i class="fa fa-circle-o o" aria-hidden="true"></i> wins!</h2>',
+    },
+    tie: {
+      msg: '<h2><i class="fa fa-star-half-o" aria-hidden="true"></i> Tie game</h2>',
+    },
     passwordChangeFail: {
       class: 'warning',
       msg: '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> Unable to change password.',
